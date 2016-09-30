b03441854cb870cc19d7eddd232f8837df6fbc6b
malte Goldenbaum <info@webklex.com>
2016-09-30 16:33:11 +0200

    Symfony Translator component added

--- src/Translator/Translator.php
+++ src/Translator/Translator.php
@@ -12,9 +12,10 @@
 
 namespace Webklex\Translator;
 
+use Symfony\Component\Translation\TranslatorInterface;
 use Webklex\Translator\Handlers\HandlerInterface;
 
-class Translator
+class Translator implements TranslatorInterface
 {
     /**
      * The handler implementation.
@@ -63,6 +64,41 @@ class Translator
         }
     }
 
+
+    /**
+     * Translates the given message.
+     *
+     * @param string      $id         The message id (may also be an object that can be cast to string)
+     * @param array       $parameters An array of parameters for the message
+     * @param string|null $domain     The domain for the message or null to use the default
+     * @param string|null $locale     The locale or null to use the default
+     *
+     * @return string The translated string
+     *
+     * @throws \InvalidArgumentException If the locale contains invalid characters
+     */
+    public function trans($id, array $parameters = array(), $domain = null, $locale = null){
+        return $this->get($id, $locale);
+    }
+
+    /**
+     * Translates the given choice message by choosing a translation according to a number.
+     *
+     * @param string      $id         The message id (may also be an object that can be cast to string)
+     * @param int         $number     The number to use to find the indice of the message
+     * @param array       $parameters An array of parameters for the message
+     * @param string|null $domain     The domain for the message or null to use the default
+     * @param string|null $locale     The locale or null to use the default
+     *
+     * @return string The translated string
+     *
+     * @throws \InvalidArgumentException If the locale contains invalid characters
+     */
+    public function transChoice($id, $number, array $parameters = array(), $domain = null, $locale = null){
+        return $this->get($id, $locale);
+    }
+
+
     /**
      * Determine if a translation exists.
      *
@@ -98,7 +134,7 @@ class Translator
             $this->setMissing($key, $locale);
             return $key;
         }
-        
+
         return $this->languages[$locale][$key]["value"];
     }
 
